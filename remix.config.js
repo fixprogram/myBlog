6ba464f87a02847d83351988067d2843f9e78aaa
Denis <fixprogram@mail.ru>
2022-05-13 12:41:44 +0300

    remix config changes for netlify

--- remix.config.js
+++ remix.config.js
@@ -3,5 +3,12 @@
  */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  ignoredRouteFiles: [".*", "**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverBuildTarget: "netlify",
+  server: "./server.js",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "netlify/functions/server/index.js",
+  publicPath: "/build/",
+  devServerPort: 8002,
 };
